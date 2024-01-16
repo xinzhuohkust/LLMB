7c81aa9d441be9e70c0a9b1176ce58bf5696bc38
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2024-01-16 22:09:53 +0800

    ()

--- R/keyword_search.R
+++ R/keyword_search.R
@@ -1,26 +1,9 @@
-engine <- V8::v8()
-
-engine$source("./inst/js/crypto-js.js")
-
-engine$source("./inst/js/signature.js")
-
-signature <- purrr::possibly(
-  \(json, url = "/v2/threads/search?sortType=0") {
-    engine$eval(sprintf('var e = "%s";', url))
-    engine$call("generate_signature", json)
-  },
-  otherwise = message("\tload signature functions failed.")
-)
-
-json_to_tibble <- \(x) {
-  httr2::resp_body_json(x) |>
-    purrr::pluck("data", "data") |>
-    purrr::map_dfr(tibble::as_tibble) |>
-    dplyr::mutate(dplyr::across(dplyr::contains("Date"), ~ as.POSIXct(., origin = "1970-01-01", tz = "Asia/Shanghai")))
-}
 
 keyword_search <- purrr::possibly(
   \(keywords = "上访", page = 1, rows = 50, format = "tibble", sleep = sample(seq(1, 2, 0.05), 1)) {
+
+    cryptojs_env <- get(".cryptojs_env", envir = asNamespace("LLMB"))
+
     param <- sprintf(
       '{"position":0,"keywords":"%s","fid":null,"domainId":null,"typeId":null,"timeRange":null,"ansChecked":false,"stTime":null,"sortType":"0","page":%s,"rows":%s}',
       keywords,
@@ -51,7 +34,7 @@ keyword_search <- purrr::possibly(
         backoff = ~5
       ) |>
       httr2::req_body_raw(
-        sprintf('{"appCode":"PC42ce3bfa4980a9","token":"","signature":"%s","param":"{\\"position\\":0,\\"keywords\\":\\"%s\\",\\"fid\\":null,\\"domainId\\":null,\\"typeId\\":null,\\"timeRange\\":null,\\"ansChecked\\":false,\\"stTime\\":null,\\"sortType\\":\\"0\\",\\"page\\":%s,\\"rows\\":%s}"}', signature(param), keywords, page, rows)
+        sprintf('{"appCode":"PC42ce3bfa4980a9","token":"","signature":"%s","param":"{\\"position\\":0,\\"keywords\\":\\"%s\\",\\"fid\\":null,\\"domainId\\":null,\\"typeId\\":null,\\"timeRange\\":null,\\"ansChecked\\":false,\\"stTime\\":null,\\"sortType\\":\\"0\\",\\"page\\":%s,\\"rows\\":%s}"}', cryptojs_env$signature(param), keywords, page, rows)
       ) |>
       httr2::req_perform()
 
@@ -60,7 +43,7 @@ keyword_search <- purrr::possibly(
     if (httr2::resp_status(result) == 200) {
       switch(format,
         "string" = httr2::resp_body_string(result),
-        "tibble" = json_to_tibble(result),
+        "tibble" = cryptojs_env$json_to_tibble(result),
         "JSON" = httr2::resp_body_json(result),
         stop("Only three avilable output formats for the results: JSON, tibble or string")
       )
@@ -69,7 +52,5 @@ keyword_search <- purrr::possibly(
     }
   }
 )
-
-
 
 
---
+++ R/zzz.R
@@ -0,0 +1,33 @@
+.onLoad <- function(libname, pkgname) {
+
+  cryptojs_env <- new.env(parent = emptyenv())
+
+
+  cryptojs_env$engine <- V8::v8()
+
+  cryptojs_env$engine$source("./inst/js/crypto-js.js")
+
+  cryptojs_env$engine$source("./inst/js/signature.js")
+
+  cryptojs_env$signature <- purrr::possibly(
+    \(json, url = "/v2/threads/search?sortType=0") {
+      cryptojs_env$engine$eval(sprintf('var e = "%s";', url))
+      cryptojs_env$engine$call("generate_signature", json)
+    },
+    otherwise = message("\tload signature functions failed.")
+  )
+
+  cryptojs_env$json_to_tibble <- \(x) {
+    httr2::resp_body_json(x) |>
+      purrr::pluck("data", "data") |>
+      purrr::map_dfr(tibble::as_tibble) |>
+      dplyr::mutate(dplyr::across(dplyr::contains("Date"), ~ as.POSIXct(., origin = "1970-01-01", tz = "Asia/Shanghai")))
+  }
+
+  ns <- asNamespace(pkgname)
+
+  assign(".cryptojs_env", cryptojs_env, envir = ns)
+
+  message("crypto-js has been loaded into R.")
+}
+
